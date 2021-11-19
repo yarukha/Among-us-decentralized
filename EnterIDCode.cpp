cb698b91fb824a621e4417c0fb2e3495a77540a7
yarukha <fxdx@DESKTOP-87TABR6.localdomain>
2021-11-19 16:56:43 +0100

    ar

--- EnterIDCode.cpp
+++ EnterIDCode.cpp
@@ -92,7 +92,7 @@ void onCloseEnterIDCode() {
 }
 
 void resetCode() {
-    for(quint i = 0 i< DIGIT_NUMBER; i++){
+    for(quint8 i = 0 i< DIGIT_NUMBER; i++){
         code[i] = DIGIT_UNDEFINED;
     }
 }
