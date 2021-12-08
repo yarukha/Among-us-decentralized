436986e7bd32fea5a582f0fd3fff82eafcd3d888
yarukha <pub.myself666@gmail.com>
2021-12-08 02:47:57 +0100

    hk

--- EnterIDCode.cpp
+++ EnterIDCode.cpp
@@ -33,6 +33,7 @@ QString getStringofcode(){
     return string_code;
 }
 
+//generates the painter and draw the code on the card
 QPair<QPixmap*, QPainter*> getEnterIDCodePixmapPainter()
 {
     QPixmap* pixmap = new QPixmap(EnterIDCodeBackgroundPixmap->size());
@@ -41,11 +42,12 @@ QPair<QPixmap*, QPainter*> getEnterIDCodePixmapPainter()
     
     QString s = getStringofcode(); 
     QFont font = painter->font();
-    font->setPixelSize(50);
-    painter->setFont(font)
-    painter-> drawText(300, 650, 100, 50,0,s);
+    font.setPixelSize(28);
+    painter->setFont(font);
+    painter-> drawText(260, 660, 100, 50,0,s);
     return qMakePair(pixmap, painter);
 }
+
 
 QLabel* getEnterIDCode(){
     if(!EnterIDCodeBackgroundPixmap) {EnterIDCodeBackgroundPixmap = getQPixmap("EnterIdCode_resized.png");};
@@ -84,6 +86,76 @@ QLabel* getEnterIDCode(){
 
 } 
 
+//return the digit clicked on the panel
+quint8 DigitClicked(quint16 x, quint16 y)
+{
+    if (x>X_TOP_LEFT_1 && x < X_TOP_LEFT_1 + X_LENGTH && y < Y_TOP_LEFT_1 && y >Y_TOP_LEFT_1 + Y_LENGTH){
+        return 1;
+    }
+    else if(x>X_TOP_LEFT_1+ X_LENGTH +X_SPACE && x<X_TOP_LEFT_1+ X_LENGTH +2*X_SPACE && y < Y_TOP_LEFT_1 && y >Y_TOP_LEFT_1 + Y_LENGTH){
+        return 2;
+    }
+    else if(x>X_TOP_LEFT_1+ 2*X_LENGTH +2*X_SPACE && x<X_TOP_LEFT_1+ 2*X_LENGTH +3*X_SPACE && y < Y_TOP_LEFT_1 && y >Y_TOP_LEFT_1 + Y_LENGTH){
+        return 3;
+    }
+    else if(x>X_TOP_LEFT_1 && x < X_TOP_LEFT_1 + X_LENGTH&& y < Y_TOP_LEFT_1+Y_SPACE+Y_LENGTH && y >Y_TOP_LEFT_1 + Y_SPACE + 2*Y_LENGTH){
+        return 4;
+    }
+    else if(x>X_TOP_LEFT_1+ X_LENGTH +X_SPACE && x<X_TOP_LEFT_1+ X_LENGTH +2*X_SPACE && y < Y_TOP_LEFT_1+Y_SPACE+Y_LENGTH && y >Y_TOP_LEFT_1 + Y_SPACE + 2*Y_LENGTH){
+        return 5;
+    }
+    else if(x>X_TOP_LEFT_1+ 2*X_LENGTH +2*X_SPACE && x<X_TOP_LEFT_1+ 2*X_LENGTH +3*X_SPACE && y < Y_TOP_LEFT_1+Y_SPACE+Y_LENGTH && y >Y_TOP_LEFT_1 + Y_SPACE + 2*Y_LENGTH){
+        return 6;
+    }
+    else if(x>X_TOP_LEFT_1 && x < X_TOP_LEFT_1 + X_LENGTH && y < Y_TOP_LEFT_1+2*Y_SPACE+2*Y_LENGTH && y >Y_TOP_LEFT_1 + 2*Y_SPACE + 3*Y_LENGTH){
+        return 7;
+    }
+    else if(x>X_TOP_LEFT_1+ X_LENGTH +X_SPACE && x<X_TOP_LEFT_1+ X_LENGTH +2*X_SPACE && y < Y_TOP_LEFT_1+2*Y_SPACE+2*Y_LENGTH && y >Y_TOP_LEFT_1 + 2*Y_SPACE + 3*Y_LENGTH){
+        return 8;
+    }
+    else if(x>X_TOP_LEFT_1+ 2*X_LENGTH +2*X_SPACE && x<X_TOP_LEFT_1+ 2*X_LENGTH +3*X_SPACE && y < Y_TOP_LEFT_1+2*Y_SPACE+2*Y_LENGTH && y >Y_TOP_LEFT_1 + 2*Y_SPACE + 3*Y_LENGTH){
+        return 9;
+    }
+    else if(x>X_TOP_LEFT_1+ X_LENGTH +X_SPACE && x<X_TOP_LEFT_1+ X_LENGTH +2*X_SPACE && y < Y_TOP_LEFT_1+3*Y_SPACE+3*Y_LENGTH && y >Y_TOP_LEFT_1 + 3*Y_SPACE + 4*Y_LENGTH){
+        return 0;
+    }
+    else return 10;
+}
+
+//write ith digit clicked
+void WriteDigit(QPainter* painter, quint16 position, quint16 digit){
+    QString s;
+    QTextStream(& s)<< digit;
+
+    QFont font = painter->font();
+    font.setPixelSize(40);
+    painter->setFont(font);
+    painter->drawText(390,440,s);
+}
+
+
+void onMouseEventEnterIDCode(QMouseEvent* mouseEvent)
+{
+    QPair<QPixmap*, QPainter*> pixmapPainter = getFixWiringPixmapPainter();
+    QPixmap* qBackgroundPixmap = pixmapPainter.first;
+    QSize pixmapSize = qBackgroundPixmap->size(),
+          windowSize = inGameUI->size();
+    QPainter* painter = pixmapPainter.second;
+    QPoint position = mouseEvent->pos();
+    qint16 mouseY = position.y() - (windowSize.height() - pixmapSize.height()) / 2,
+           mouseX = position.x() - (windowSize.width() - pixmapSize.width()) / 2;
+    if(mouseX < 0 || mouseX >= pixmapSize.width() || mouseY < 0 || mouseY >= pixmapSize.height())
+        return;
+    quint16 digit = DigitClicked(mouseX,mouseY);
+    if (digit<10){
+        WriteDigit(painter, 0, digit);
+        return;
+    }
+    else {return;}
+
+}
+
+
 void onCloseEnterIDCode() {
     playSound("Fix_Wiring_task_close_sound.wav");
     resetCode();
@@ -96,6 +168,7 @@ void onCloseEnterIDCode() {
         currEnterIDCodePixmap = nullptr;
     }
 }
+
 
 void resetCode() {
     for(quint8 i = 0; i< DIGIT_NUMBER; i++){
--- EnterIDCode.h
+++ EnterIDCode.h
@@ -10,6 +10,15 @@
 #define MAX_DIGIT 9
 #define DIGIT_UNDEFINED MAX_DIGIT +1
 
+#define X_TOP_LEFT_1 277
+#define Y_TOP_LEFT_1 67
+#define X_LENGTH 66
+#define Y_LENGTH 66
+#define X_SPACE 18
+#define Y_SPACE 12
+
+
+
 QPair<QPixmap*, QPainter*> getEnterIDCodePixmapPainter();
 QLabel* getEnterIDCode();
 
